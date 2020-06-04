bd12f79824626c0293f0202523c93fdbf06d0956
leisyarcia <leisyarcia@gmail.com>
2020-06-04 00:05:15 +0000

    2nd commit

--- src/js/index.js
+++ src/js/index.js
@@ -36,7 +36,7 @@ function render(variables = {}) {
           <h1>${variables.name + ", " + variables.lastname}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city + ", " + variables.country} </h3>
-          <ul class="position-right">
+          <ul class=${variables.socialMediaPosition}"position-right">
             <li><a href="${
               variables.twitter
             }"><i class="fa fa-twitter"></i></a></li>
